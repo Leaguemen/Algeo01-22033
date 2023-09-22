b44f6a2e606c6bf064f0ea0b24898dc82a78d254
Bryan Cornelius Lauwrence <bryan.cornelius.lauw@gmail.com>
2023-09-23 00:41:01 +0700

    ADT bryan

--- Matrix.java
+++ Matrix.java
@@ -1,9 +1,11 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+
+import javax.swing.text.html.parser.Element; 
 public class Matrix {
     final int ROW_CAP = 100;
     final int COL_CAP = 100;
 
-    public int[][] memory;
+    public float[][] memory;
     public int rowEff;
     public int colEff;
     
@@ -11,7 +13,7 @@ public class Matrix {
     public Matrix(int rowEff,int colEff){
         this.rowEff = rowEff;
         this.colEff = colEff;
-        this.memory = new int[ROW_CAP][COL_CAP];
+        this.memory = new float[ROW_CAP][COL_CAP];
     }
 
     //Methods
@@ -46,10 +48,133 @@ public class Matrix {
         }
     }
 
-    public static void main(String[] args){
-        Matrix m = new Matrix(3, 3);
-        readMatrix(m,3,3);
-        displayMatrix(m);
+    public static boolean isIdxEff(Matrix m, int i, int j) {
+        return (i <= getLastIdxRow(m) && j <= getLastIdxCol(m));
     }
 
+    public static void copyMatrix(Matrix mIn, Matrix mOut) {
+        mOut.rowEff = mIn.rowEff;
+        mOut.colEff = mIn.colEff;
+
+        int i, j;
+        for (i = 0; i < mOut.rowEff; i++) {
+            for (j = 0; j < mIn.rowEff; j++) {
+                mOut.memory[i][j] = mIn.memory[i][j];
+            }
+        }
+    }
+
+    public static Matrix subtractMatrix(Matrix m1, Matrix m2) {
+        int i, j;
+        
+        for (i = 0; i <= getLastIdxRow(m2); i++) {
+            for (j = 0; j <= getLastIdxCol(m2); j++) {
+                m1.memory[i][j] -= m2.memory[i][j];
+            }
+        }
+        return m1;
+    }
+
+    public static Matrix multiplyMatrix(Matrix m1, Matrix m2) {
+        Matrix m = new Matrix(m1.rowEff, m2.colEff);
+        int i, j, k;
+
+        for (i = 0; i <= getLastIdxRow(m); i++) {
+            for (j = 0; j <= getLastIdxCol(m); j++) {
+                m.memory[i][j] = 0;
+                for (k = 0; k <= getLastIdxCol(m1); k++) {
+                    m.memory[i][j] += m1.memory[i][k]*m2.memory[k][j];
+                }
+            }
+        }
+        return m;
+    }
+
+    public static Matrix multiplyMatrixWithMod(Matrix m1,Matrix m2,int mod) {
+        Matrix m = new Matrix(m1.rowEff, m2.colEff);
+        int i, j, k;
+
+        for (i = 0; i <= getLastIdxRow(m); i++) {
+            for (j = 0; j <= getLastIdxCol(m); j++) {
+                m.memory[i][j] = 0;
+                for (k = 0; k <= getLastIdxCol(m1); k++) {
+                    m.memory[i][j] += m1.memory[i][k]*m2.memory[k][j];
+                }
+                m.memory[i][j] %= mod;
+            }
+        }
+        return m;
+    }
+
+    public static boolean isMatrixEqual(Matrix m1, Matrix m2) {
+        boolean sama = false;
+        int i = 0;
+        int j;
+        if (m1.rowEff == m2.rowEff && m1.colEff == m2.colEff) {
+            sama = true;
+            while (sama && i <= getLastIdxRow(m2)) {
+                j = 0;
+                while (sama && j <= getLastIdxCol(m2)) {
+                    if (m1.memory[i][j] != m2.memory[i][j]) {
+                        sama = false;
+                    }
+                    j++;
+                }
+                i++;
+            }
+        }
+        return sama;
+    }
+
+    public static boolean isMatrixNotEqual(Matrix m1, Matrix m2){
+        return !isMatrixEqual(m1,m2);
+    }
+
+    public static boolean isSquare(Matrix m){ //bryan
+        return (m.colEff == m.rowEff);
+    }
+
+    public static boolean isSymmetric(Matrix m) {
+        boolean simetri = false;
+        int i, j;
+
+        if (isSquare(m)) {
+            simetri = true;
+            i = 0;
+            while (simetri && i < getLastIdxRow(m)) {
+                j = i+1;
+                while (simetri && j <= getLastIdxCol(m)) {
+                    if (m.memory[i][j] != m.memory[j][i]) {
+                        simetri = false;
+                    }
+                }
+            }
+        }
+        return simetri;
+    }
+
+    public static Matrix transpose(Matrix m) {
+        Matrix mNew = new Matrix(m.colEff, m.rowEff);
+        int i, j;
+
+        for (i = 0; i <= getLastIdxRow(mNew); i++) {
+            for (j = 0; j <= getLastIdxCol(mNew); j++) {
+                mNew.memory[i][j] = m.memory[j][i];
+            }
+        }
+        return mNew;
+    }
+
+    public static void pTranspose(Matrix m) {
+        int i, j;
+        float temp;
+
+        for (i = 0; i <= getLastIdxRow(m); i++) {
+            for (j = i+1; j <= getLastIdxCol(m); j++) {
+                temp = m.memory[i][j];
+                m.memory[i][j] = m.memory[j][i];
+                m.memory[j][i] = temp;
+            } 
+        }
+    }
 }
