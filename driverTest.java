11ef7c378948a71a3ec21ff11e544648d6f56652
Bryan Cornelius Lauwrence <bryan.cornelius.lauw@gmail.com>
2023-09-23 23:33:14 +0700

    Parse

--- driverTest.java
+++ driverTest.java
@@ -1,8 +1,15 @@
-import ADT_Matrix.Matrix;
 public class driverTest {
-        public static void main(String[] args){
-        Matrix m = new Matrix(3, 3);
-        Matrix.readMatrix(m,3,3);
-        Matrix.displayMatrix(m);
+        public static void main(String[] args) throws Exception{
+        // Matrix m = new Matrix(3, 3);
+        // Matrix.readMatrix(m,3,3);
+        // Matrix.displayMatrix(m);
+        ADT_Matrix.Matrix m = new ADT_Matrix.Matrix(0, 0);
+        String fileName = "*";
+
+        fileName = ADT_Matrix.ReadFile.pilihFile(fileName);
+        m = ADT_Matrix.ReadFile.parseFile(m,fileName);
+        ADT_Matrix.Matrix.displayMatrix(m);
+        System.out.println(m.colEff);
+        System.out.println(m.rowEff);
     }
 }
