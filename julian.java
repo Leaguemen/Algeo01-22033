5679f08f2c9c01ff8d991c1849dceef0b435251f
Bryan Cornelius Lauwrence <bryan.cornelius.lauw@gmail.com>
2023-10-05 13:12:28 +0700

    fixed read

--- julian.java
+++ julian.java
@@ -1,18 +1,26 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class julian {
     public static void main(String[] args) {
-        System.out.println("------------------PROGRAM KELUAR-----------------");
+        // System.out.println("------------------PROGRAM KELUAR-----------------");
         
-        System.out.println("==================PROGRAM KELUAR=================");
-        Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        int y = in.nextInt();
+        // System.out.println("==================PROGRAM KELUAR=================");
+        // Scanner in = new Scanner(System.in);
+        // int x = in.nextInt();
+        // int y = in.nextInt();
 
-        System.out.println(x + " " + y);
+        // System.out.println(x + " " + y);
 
-        float[] a = {};
-        System.out.println(a.length);
-
+        // float[] a = {};
+        // System.out.println(a.length);
+        File currentDir = new File(".");
+        File parentDir = currentDir.getParentFile();
+        File directory = new File (parentDir,"Regresi");
+        for (File file : directory.listFiles()) {
+            if (file.isFile() && file.getName().equals("Regresi.java")) {
+                System.out.println("yey");
+            }
+        }
     }
 }
